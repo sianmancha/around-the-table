042f4538db33e70b8cfacebeea69f85915213763
sianmancha <manchasian@gmail.com>
2024-01-23 12:57:09 -0600

    Fixes API error in the CreateAccountForm.

--- components/CreateAccountForm.tsx
+++ components/CreateAccountForm.tsx
@@ -24,37 +24,10 @@ const SignUpSchema = Yup.object().shape({
 })
 
 export default function CreateAccountForm({onClose}: Props) {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-
     const router = useRouter();
 
     const handleCreateAccount = async (values: Values) => {
-
-        if(!name || !email || !password) {
-            setError("All fields are required")
-            return;
-        }
-
         try {
-
-            const resUserExists = await fetch('api/userExists', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json", 
-                },
-                body: JSON.stringify({ email: values.email })
-            });
-
-            const { user } = await resUserExists.json();
-
-            if (user) {
-                setError("User already exists.")
-                return;
-            }
-
             const res = await fetch('api/register', {
                 method: "POST",
                 headers: {
@@ -78,29 +51,6 @@ export default function CreateAccountForm({onClose}: Props) {
     }
 
     return (
-        // <div className="grid place-items-center h-screen">
-        //     <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-        //         <h1 className="text-xl font-bold my-4">
-        //             Register
-        //         </h1>
-        //         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        //             <input onChange={e => setName(e.target.value)} type="text" placeholder="Full Name" />
-        //             <input onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" />
-        //             <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
-        //             <button className="bg-green-600 text-white font-bld cursor-pointer px-6 py-2">
-        //                 Register
-        //             </button>
-        //             {error && (
-        //                 <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-        //                     {error}
-        //                 </div>
-        //             )}
-        //             <Link className="text-sm mt-3 text-right" href={'/'}>
-        //                 Already have an account? <span className="underline">Login</span>
-        //             </Link>
-        //         </form>
-        //     </div>
-        // </div>
         <Formik
                 initialValues={{name: '', email: '', password: '', confirmPassword: ''}}
                 validationSchema={SignUpSchema}
