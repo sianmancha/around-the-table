fcf8b541cec0d0cc65b13407535ba58aed7f60f2
sianmancha <manchasian@gmail.com>
2024-01-25 20:06:30 -0600

    Adds styling to signed out navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,18 +2,28 @@
 
 import diningTable from '../app/dining-table.svg'
 import Image from 'next/image'
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 
 
 export default function NavBar() {
     const { data: session } = useSession();
+
     return (
-        <div className=" flex items-center justify-between bg-[#D4AC97] p-4">
+        <div className="flex items-center justify-between bg-[#D4AC97] py-4 px-8">
             <Image alt='Around the Table' src={diningTable} width={60} height={60} />
             {session ? (
-                <div>Signed In</div>
+                <div>
+                    <button onClick={() => signOut()}>Sign Out</button>
+                </div>
             ) : (
-                <div>Signed Out</div>
+                <div className='flex flex-grow items-center'>
+                    <h1 className='text-4xl text-[#FAF9F6] ml-auto'>
+                        Around the Table
+                    </h1>
+                    <div className='text-[#FAF9F6] ml-auto'>
+                        Signed Out
+                    </div>
+                </div>
             )}
         </div>
     )
